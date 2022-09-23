205b5aa68efe6118ac8de7ed9a447f3b9b60299a
Jesper Lundgren <koudelkaa@gmail.com>
2022-09-23 23:03:33 +0200

    wip

--- packages/client/tests/test.ts
+++ packages/client/tests/test.ts
@@ -4,8 +4,6 @@ import {TKBuilder} from '@type-knit/server'
 import type { ToClient } from '@type-knit/server'
 import { z } from "zod";
 import {createClient} from '../src/index'
-import { Request } from 'node-fetch'
-const polka = require('polka');
 
 const User = z.object({
   username: z.string(),
@@ -18,34 +16,13 @@ let tkr = tk.router({
 
 import { createServerAdapter } from '@whatwg-node/server'
 
+const server = http.createServer(createServerAdapter((req) => {
+  return tkr.route({req})
+}));
+
 const serverStart = new Promise((resolve, reject) => {
-polka()
-  .post('*', createServerAdapter((req) => {
-    console.log("HERE I AM")
-    return tkr.route({req})
-  }))
-  .listen(3000, () => {
-    console.log(`> Running on localhost:3000`);
-    resolve('ok')
-  });
+  server.listen(3000, '127.0.0.1', () => resolve('ok')).once('error', (err) => reject(err))
 })
-//const server = http.createServer(createServerAdapter((req) => {
-//  return tkr.route({req})
-//}));
-
-
-function sleep(ms: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-//server.listen()
-
-//new Promise((resolve, reject) => {
-//  server.listen(12301, '127.0.0.1', () => resolve('ok')).once('error', (err) => reject(err))
-//})
-
 
 type MyServer = ToClient<typeof tkr>
 let client = createClient<MyServer>("http://127.0.0.1:3000")
@@ -54,12 +31,12 @@ describe('sum module', () => {
     await serverStart
   })
   test('handle simple call', async () => {
-    await serverStart
-    console.log('in test')
     let r = await client.e().hello.call({username: "TK"})
     expect(r).toBe("Hello from TK!")
   });
-  afterAll(() => {
-    //server.close()
+  afterAll(async () => {
+    await new Promise((resolve, reject) => {
+      server.close((err) => err ? reject(err) : resolve('closed'))
+    })
   })
 });
