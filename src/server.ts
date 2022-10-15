fe7884892e93acc048bfd698fa6463d9eff9a3a8
Jesper Lundgren <koudelkaa@gmail.com>
2022-10-15 12:55:01 +0200

    alpha

---
+++ src/server.ts
@@ -0,0 +1,300 @@
+import { createClient, ToBase, ToClient } from "./client";
+import {
+  Emitter,
+  EventEmitter,
+  eventStream,
+  StreamEvent,
+  Topics,
+} from "./events";
+
+type Sameish<T, U> = [T] extends [U] ? ([U] extends [T] ? T : U extends unknown ? T : never) : T extends unknown ? U : never;
+export type MaybeAsync<T> = T | PromiseLike<T>
+
+export interface Parsable {
+  parse(obj: unknown): any
+}
+
+type ParseType<T> = T extends Parsable ? ReturnType<T['parse']> : never;
+
+function parseArgs<T extends Parsable>(args: unknown, schema: T): { ok: true, data: ParseType<T> } | { ok: false, error: Response } {
+  let data;
+  try {
+    data = schema.parse(args)
+  } catch (error) {
+    return { ok: false, error: new Response(JSON.stringify(error), { status: 400 }) }
+  }
+  return { ok: true, data }
+}
+
+export type TKServerContext = Omit<Record<string, any>, "req"> & {
+  req: Request;
+};
+
+export type TKOK<T> = { ok: true, data: T }
+export type TKERR = { ok: false, error: string, status?: number }
+export function tkok<T>(data: T): TKOK<T> {
+  return { ok: true, data }
+}
+export function tkerr(error: string, status?: number): TKERR { return { ok: false, error, status } }
+
+export type TKResult<T> = TKOK<T> | TKERR
+export type StreamReturn<V, T extends Topics, Ts extends keyof T> = { topic: Ts, initValue?: Sameish<V, T[Ts]> }
+export type TKStreamResult<V, T extends Topics, Ts extends keyof T> =
+  | TKOK<StreamReturn<V, T, Ts>>
+  | TKERR;
+
+export function tkstream<T>
+    (topic: string, initValue?: T) { 
+  return tkok({topic, initValue })
+}
+
+export type Instance<
+  R extends Router = any,
+  SchemaType extends Parsable = any,
+  In = any,
+  Ctx extends TKServerContext = any
+> = {
+  _type: "instance";
+  _schema?: SchemaType;
+  _middlewares: MiddleWare[];
+  instance: (
+    args: In,
+    ctx: Ctx
+  ) => Promise<{ fetch: (req: Request) => Promise<Response> }>;
+};
+
+export type Call<
+  SchemaType extends Parsable = any,
+  In = any,
+  Out = any,
+  Ctx extends TKServerContext = any
+> = {
+  _type: "call";
+  _schema?: SchemaType;
+  _middlewares: MiddleWare[];
+  call: (args: In, ctx: Ctx) => MaybeAsync<TKResult<Out>>;
+};
+
+export type Stream<
+  SchemaType extends Parsable = any,
+  In = any,
+  Out = any,
+  Ctx extends TKServerContext = any,
+  T extends Topics = any,
+  Ts extends keyof T = any
+> = {
+  _type: "stream";
+  _schema: SchemaType;
+  _middlewares: MiddleWare[];
+  stream: (args: In, ctx: Ctx) => MaybeAsync<TKStreamResult<Out, T, Ts>>;
+};
+
+export type MiddleWare<Ctx extends TKServerContext = any> = {
+  handle: (
+    ctx: Ctx
+  ) => { type: "response"; data: Response } | { type: "ctx"; data: Ctx };
+};
+export type Router<Ctx extends TKServerContext = any> = {
+  _type: "router";
+  _middlewares: MiddleWare[];
+  route: (ctx: Ctx & MaybeTKInternals, prefix?: string) => Promise<Response>;
+};
+
+export type TKInternalKeys = "_type" | "_schema" | "_middlewares" | "route" | "instance" | "call" | "stream" | "tkclient";
+type Routes<Ctx extends TKServerContext = any> = {
+  [key: string]: Call | Stream | Router<Ctx> | Instance | Routes<Ctx>;
+};
+
+type TKRequest = {
+  args: unknown[];
+};
+
+type TKInternals = {
+  index: number;
+  paths: string[];
+  tkreq: TKRequest;
+};
+
+type MaybeTKInternals = {
+  __tk_internals?: TKInternals;
+};
+
+interface Fetch {
+  fetch: (req: Request) => Promise<Response>
+}
+
+// Build API
+export class TKBuilder<
+  Ctx extends TKServerContext,
+  T extends Topics = Record<string, any>
+> {
+  private readonly emitter: Emitter<T>;
+  constructor(emitter?: Emitter<T>) {
+    this.emitter = emitter || new EventEmitter<T>();
+  }
+  emit<Ts extends keyof T>(topic: Ts, event: StreamEvent<T[Ts]>): void {
+    this.emitter.emit(topic, event)
+  }
+  instance<R extends Router = any, SchemaType extends Parsable = any>(
+    router: R,
+    f: (
+      args: ParseType<SchemaType>,
+      ctx: Ctx
+    ) => MaybeAsync<Fetch>,
+    schema?: SchemaType,
+    middlewares: MiddleWare<Ctx>[] = []
+  ): Instance<R, Parsable, ParseType<SchemaType>, Ctx> {
+    return {
+      _type: "instance",
+      _schema: schema,
+      _middlewares: middlewares,
+      instance: async (args: ParseType<SchemaType>, ctx: Ctx) => f(args, ctx),
+    };
+  }
+  call<SchemaType extends Parsable, Out>(
+    schema: SchemaType,
+    f: (args: ParseType<SchemaType>, ctx: Ctx) => MaybeAsync<TKResult<Out>>,
+    middlewares: MiddleWare<Ctx>[] = []
+  ): Call<Parsable, ParseType<SchemaType>, Out, Ctx> {
+    return {
+      _type: "call",
+      _schema: schema,
+      _middlewares: middlewares,
+      call: f,
+    };
+  }
+  stream<SchemaType extends Parsable, Out>(
+    schema: SchemaType,
+    f: (args: ParseType<SchemaType>, ctx: Ctx) => MaybeAsync<TKStreamResult<Out, T, keyof T>>,
+    middlewares: MiddleWare<Ctx>[] = []
+  ): Stream<Parsable, ParseType<SchemaType>, Out, Ctx, T, keyof T> {
+    return {
+      _type: "stream",
+      _schema: schema,
+      _middlewares: middlewares,
+      stream: f,
+    };
+  }
+  router<R extends Routes>(
+    routes: R,
+    prefix: string = "/",
+    middlewares: MiddleWare<Ctx>[] = [],
+  ): Router<Ctx> & R & {tkclient: ToBase<R>} {
+    prefix = prefix.endsWith('/') ? prefix : prefix + '/'
+    const route = async (ctx: Ctx & MaybeTKInternals) => {
+      for (const m of middlewares) {
+        let out = m.handle(ctx);
+        if (out.type == "response") {
+          return out.data;
+        }
+        ctx = {
+          ...out.data,
+          __tk_internals: ctx.__tk_internals,
+        };
+      }
+      if (!ctx.__tk_internals) {
+        let pathname = new URL(ctx.req.url).pathname;
+        let paths: string[] = []
+        let tkreq = { args: [] }
+        if (pathname.startsWith(prefix)) {
+          paths = pathname.replace(prefix, "").split("/");
+          tkreq = await ctx.req.json();
+          if (typeof tkreq !== "object") {
+            return new Response("bad request", { status: 400 });
+          }
+          tkreq.args = tkreq.args ? tkreq.args : [];
+          if (!Array.isArray(tkreq.args)) {
+            return new Response("bad request", { status: 400 });
+          }
+        }
+        ctx.__tk_internals = {
+          index: 0,
+          paths,
+          tkreq,
+        };
+      }
+      let path = ctx.__tk_internals.paths.shift()
+      let r: Routes | undefined = routes
+      while (path && r && r[path]) {
+        const obj = r[path];
+        switch (obj._type) {
+          case "call": {
+            const payload = ctx.__tk_internals.tkreq.args.shift();
+            const parsed = parseArgs(payload, obj._schema)
+            if (!parsed.ok) return parsed.error
+            const result = await obj.call(parsed.data, ctx);
+            if (!result.ok) {
+              const status = result.status ? result.status : 400;
+              return new Response(result.error, { status });
+            }
+            return new Response(JSON.stringify(result.data), {
+              status: 200,
+            });
+          }
+          case "stream": {
+            const payload = ctx.__tk_internals.tkreq.args.shift();
+            const parsed = parseArgs(payload, obj._schema)
+            if (!parsed.ok) return parsed.error
+            const result = await obj.stream(parsed.data, ctx);
+            if (!result.ok) {
+              const status = result.status ? result.status : 400;
+              return new Response(result.error, { status });
+            }
+            let publish: (event: StreamEvent<unknown>) => void;
+            const unsub = this.emitter.subscribe(
+              result.data.topic,
+              (event: StreamEvent<unknown>) => publish && publish(event)
+            );
+            const es = eventStream(() => unsub());
+            publish = es.publish;
+            if (result.data.initValue !== undefined) {
+              publish({ type: "data", data: result.data.initValue })
+            } else {
+              publish({type: "ping"})
+            }
+            return new Response(es.readable, {
+              status: 200,
+              headers: {
+                "Content-Type": "text/event-stream",
+                Connection: "keep-alive",
+                "Cache-Control": "no-cache",
+              },
+            });
+          }
+          case "instance": {
+            const payload = ctx.__tk_internals.tkreq.args.shift();
+            const parsed = parseArgs(payload, obj._schema)
+            if (!parsed.ok) return parsed.error
+            const fetchImpl = await obj.instance(parsed.data, ctx);
+            let url = new URL(ctx.req.url);
+            ctx.__tk_internals.paths.shift();
+            const body = JSON.stringify(ctx.__tk_internals.tkreq)
+            url.pathname = ctx.__tk_internals.paths.join('/')
+            const headers = new Headers()
+            for (const [header, value] of (ctx.req.headers as any).entries()) {
+              if(header !== 'content-length') {
+                headers.append(header, value)
+              }
+            }
+            return fetchImpl.fetch(new Request(url, { headers, method: 'POST', body  }));
+          }
+          case "router": {
+            return obj.route(ctx);
+          }
+          default: {
+            r = r[path] as Routes
+            path = ctx.__tk_internals.paths.shift()
+          }
+        }
+      }
+      return new Response("route not found", { status: 404 });
+    }
+    return {
+      ...routes,
+      _middlewares: middlewares,
+      _type: "router",
+      route,
+      tkclient: createClient<ToBase<R>>("http://localhost" + prefix, {fetch: route}).e()
+    };
+  }
+}
---
+++ tests/test.ts
@@ -0,0 +1,199 @@
+
+import { suite } from 'uvu';
+import * as assert from 'uvu/assert';
+import http from 'node:http'
+import { z } from "zod";
+import { createServerAdapter } from "@whatwg-node/server";
+import { TKBuilder, tkok, TKServerContext, tkstream } from "../src/index";
+import type { ToClient } from "../src/index";
+import { createClient } from "../src/index";
+
+const User = z.object({
+  username: z.string(),
+});
+
+const tk = new TKBuilder<TKServerContext, { testtopic: { nested: string, nr: number } }>();
+const send = (count: number) => {
+  if (count) {
+    setTimeout(() => {
+      tk.emit('testtopic', { type: "data", data: { nested: `count is: ${count}`, nr: count } })
+      send(count - 1)
+    }, 500)
+  }
+}
+
+let instanceRouter = tk.router({
+  hello: tk.call(User, (args) => tkok(`Hello ${args.username}! from instance`)),
+})
+
+
+
+const tks = new TKBuilder<TKServerContext>();
+
+let arbitraryStream = tks.router({
+  hello: tks.stream(User, (args) => {
+    if (args.username.length < 3) {
+      return tkstream("startcase", 0)
+    }
+    return tkstream("endcase", 1)
+  }),
+}, '/arbitrary')
+
+let tkr = tk.router({
+  hello: tk.call(User, (args) => tkok(`Hello ${args.username}!`)),
+  helloasync: tk.call(User, async (args) => tkok(`Hello ${args.username} async!`)),
+  hellostream: tk.stream(User, (args) => {
+    send(5)
+    return tkok({ topic: "testtopic", initValue: { nested: "initVal", nr: 6 } })
+  }),
+  helloinstance: tk.instance(instanceRouter, (args, ctx) => ({ fetch: (req: Request) => instanceRouter.route({ req }) }), User),
+  nested: {
+    hello: tk.call(User, (args) => tkok(`Hello ${args.username}! nested`))
+  }
+});
+
+let tkrp = tk.router({
+  helloprefix: tk.call(User, (args) => tkok(`Hello ${args.username}! prefixed`)),
+}, '/api')
+
+const server = http.createServer(
+  createServerAdapter((req) => {
+    let path = new URL(req.url).pathname
+    if (path.startsWith('/api')) {
+      return tkrp.route({ req })
+    }
+    if (path.startsWith('/arbitrary')) {
+      return arbitraryStream.route({ req })
+    }
+    return tkr.route({ req });
+  })
+);
+
+const serverStart = new Promise((resolve, reject) => {
+  server
+    .listen(3000, "127.0.0.1", () => resolve("ok"))
+    .once("error", (err) => reject(err));
+});
+type MyServer = ToClient<typeof tkr>;
+let client = createClient<MyServer>("http://127.0.0.1:3000");
+let prefixclient = createClient<ToClient<typeof tkrp>>("http://127.0.0.1:3000/api");
+let arbitraryclient = createClient<ToClient<typeof arbitraryStream>>("http://127.0.0.1:3000/arbitrary");
+
+const tktest = suite('type-knit');
+
+tktest.before(async () => {
+  await serverStart
+})
+
+tktest('handle prefix', async () => {
+  let res = await prefixclient.e()
+    .helloprefix
+    .call({ username: "TK" });
+  let r = res.ok ? res.data : res.error
+  assert.is(r, "Hello TK! prefixed")
+});
+
+tktest('handle nested', async () => {
+  let res = await client.e()
+    .nested
+    .hello
+    .call({ username: "TK" });
+  let r = res.ok ? res.data : res.error
+  assert.is(r, "Hello TK! nested")
+});
+
+tktest('simple call', async () => {
+  let res = await client.e()
+    .hello
+    .call({ username: "TK" });
+  let r = res.ok ? res.data : res.error
+  assert.is(r, "Hello TK!")
+});
+
+tktest('simple call async', async () => {
+  let res = await client.e()
+    .helloasync
+    .call({ username: "TK" });
+  let r = res.ok ? res.data : res.error
+  assert.is(r, "Hello TK async!")
+});
+
+tktest('simple instance call', async () => {
+  let res = await client.e()
+    .helloinstance
+    .instance({ username: "instance" })
+    .hello
+    .call({ username: "TK" });
+  let r = res.ok ? res.data : res.error
+  assert.is(r, "Hello TK! from instance")
+});
+tktest('arbitrary topic stream', async () => {
+  await new Promise(resolve => {
+    const r = arbitraryclient.e()
+      .hello
+      .stream({ username: "TK" })
+    r.start((ev) => {
+      if (ev.state === 'data') {
+        assert.is(ev.data, 0)
+        r.cancel()
+        resolve(true)
+      }
+    })
+  })
+  await new Promise(resolve => {
+    const r = arbitraryclient.e()
+      .hello
+      .stream({ username: "TKA" })
+    r.start((ev) => {
+      if (ev.state === 'data') {
+        assert.is(ev.data, 1)
+        r.cancel()
+        resolve(true)
+      }
+    })
+  })
+})
+
+tktest('simple fixed topic stream', async () => {
+  return new Promise(resolve => {
+    const r = client.e()
+      .hellostream
+      .stream({ username: "TK" })
+    const expected = [6, 5, 4, 3, 2, 1]
+    r.start((ev) => {
+      switch (ev.state) {
+        case 'connected':
+          console.log("CONNECTED")
+          break;
+        case 'connecting':
+          console.log("CONNECTING")
+          break;
+        case 'data':
+          console.log("DATA: ", ev.data)
+          const expect = expected.shift()
+          if (expected) {
+            assert.is(ev.data.nr, expect)
+          }
+          if (expected.length == 0) {
+            r.cancel()
+            resolve(true)
+          }
+          break;
+        case 'done':
+          console.log("DONE")
+        case 'reconnecting':
+          console.log("RECONNECTING")
+        default:
+          console.log("OTHER STATE")
+          break;
+      }
+    });
+  }).then(data => {
+    assert.is(data, true)
+  })
+});
+
+tktest.after(() => {
+  server.close();
+})
+tktest.run();
